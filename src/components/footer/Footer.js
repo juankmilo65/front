2436197c44278d092ef9c2e47d328afde9eadaff
Juan Camilo <jmorales@debt.com>
2022-04-20 20:55:16 +0300

    footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,9 @@ import React, {useEffect}from 'react'
 import { Layout } from 'antd';
 import { useTranslation } from "react-i18next"
 import i18next from 'i18next';
- 
+import { FacebookFilled, TwitterSquareFilled, InstagramFilled} from '@ant-design/icons';
+
+import {LanguagesWrapper} from './Footer.styles'
 
 function Footer() {
 
@@ -15,11 +17,20 @@ function Footer() {
     const { Footer } = Layout;
     
     return (
-    <Footer style={{ textAlign: 'center', color:'white', background: 'linear-gradient(to right, #4C2C89, #2a0845)' }}>
-        {t("brand")}
-        <div onClick={()=>i18next.changeLanguage('es')} >{t("spanish")}</div>
-        <div onClick={() => i18next.changeLanguage('en')} >{t("english")}</div>
-    </Footer>
+        <Footer style={{ display: 'grid', gridTemplateColumns: 'repeat(3,1fr)',  textAlign: 'center', color:'white', background: 'linear-gradient(to right, #4C2C89, #2a0845)' }}>
+            <div>
+            </div>
+            <div>
+                {t("brand")}
+            </div>
+            <LanguagesWrapper>
+                <div onClick={()=>i18next.changeLanguage('es')} ><FacebookFilled/> Facebook</div>
+                <div onClick={()=>i18next.changeLanguage('es')} ><TwitterSquareFilled/> Twitter</div>
+                <div onClick={()=>i18next.changeLanguage('es')} ><InstagramFilled/> Instagram</div>
+                <div onClick={()=>i18next.changeLanguage('es')} >{t("spanish")}</div>
+                <div onClick={() => i18next.changeLanguage('en')} >{t("english")}</div>
+            </LanguagesWrapper>
+        </Footer>
     )
 }
 
